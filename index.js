ccf554403cbedadf14aba0726eac2019ed76302f
SimpleNic <nburton@DESKTOP-E8LVOC4.localdomain>
2023-12-01 01:06:14 -0700

    completed game portion of chess

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const __publicPath = join(__dirname, "public");
 app.use(express.static(__publicPath));
 
+//
+// Server side checks
+//
+var turn = "W";
+var checkmated = "_";
 var pawn_can_be_enpassant = {
   x: NaN,
   y: NaN,
@@ -44,12 +49,12 @@ const chess_board = [
   ["__","__","__","__","__","__","__","__"],
   ["WP","WP","WP","WP","WP","WP","WP","WP"],
   ["WR","WN","WB","WQ","WK","WB","WN","WR"],
-]
+];
 
 // Checks if move is valid
 // ghost_piece, x_dest, and y_dest are relative coordinates, 
 // white has same relative and absolute coordinates, but black has it fliped
-function move(ghost_piece, x_dest, y_dest){
+function move(ghost_piece, x_dest, y_dest, real_move = true){
   let moved = false;
   let color = ghost_piece.piece[0];
   let x_dist = ghost_piece.x-x_dest;
@@ -57,21 +62,22 @@ function move(ghost_piece, x_dest, y_dest){
   let start_board = getBoardCoords(color, ghost_piece.x, ghost_piece.y);
   let dest_board = getBoardCoords(color, x_dest, y_dest);
 
-  if(!isSpacesEmpty(color, start_board, dest_board) || isSelfCheck(color, start_board, dest_board)) return false;
+  if(!isSpacesEmpty(color, start_board, dest_board) || isSelfCheck(ghost_piece.piece, start_board, dest_board)) return false;
 
   switch (ghost_piece.piece[1]){
     case "P":
-      moved = movePawn(ghost_piece.y, x_dist, y_dist, dest_board);
+      moved = movePawn(ghost_piece.y, x_dist, y_dist, dest_board, real_move);
       break;
     case "N":
-      moved = moveKnight(x_dist, y_dist, dest_board);
+      moved = moveKnight(x_dist, y_dist);
       break;
     case "B":
-      moved = moveBishop(x_dist, y_dist, dest_board);
+      moved = moveBishop(x_dist, y_dist);
       break;
     case "R":
-      moved = moveRook(x_dist, y_dist, dest_board);
-      if(moved){
+      moved = moveRook(x_dist, y_dist);
+      // remove castle options if rook moved
+      if(moved && real_move){
         if(color == "W") {
           if(start_board.x == 0) white_castle.left = false; 
           else if(start_board.x == 7) white_castle.right = false;
@@ -83,11 +89,12 @@ function move(ghost_piece, x_dest, y_dest){
       }
       break;
     case "Q":
-      moved = moveQueen(x_dist, y_dist, dest_board);
+      moved = moveQueen(x_dist, y_dist);
       break;
     case "K":
-      moved = moveKing(x_dist, y_dist, dest_board);
-      if(moved){
+      moved = moveKing(x_dist, y_dist, color, real_move);
+      // remove castle options if king moved
+      if(moved && real_move){
         if(color == "W") {
           white_castle.left = false; 
           white_castle.right = false;
@@ -99,13 +106,13 @@ function move(ghost_piece, x_dest, y_dest){
       }
       break;
   }
-  if(moved) pawn_can_be_enpassant.turn_ctr--; 
   return moved;
 }
 
-
-
-function movePawn(y_start, x_dist, y_dist, dest_board){
+// movePawn is the strangest due to all the special things it can do,
+// real_move is used when you want to see if a pawn can do something
+// without updating pawn_can_be_enpassant
+function movePawn(y_start, x_dist, y_dist, dest_board, real_move = true){
   // Simple move forward
   if(x_dist == 0 && y_dist == 1 && chess_board[dest_board.y][dest_board.x] == "__") return true;
 
@@ -113,53 +120,87 @@ function movePawn(y_start, x_dist, y_dist, dest_board){
   if(Math.abs(x_dist) == 1 && y_dist == 1 && chess_board[dest_board.y][dest_board.x] != "__") return true;
 
   // En passant
-  if(Math.abs(x_dist) == 1 && y_dist == 1 && pawn_can_be_enpassant.turn_ctr == 0 && chess_board[dest_board.y][dest_board.x] != "__")
+  if(Math.abs(x_dist) == 1 && y_dist == 1 && 
+    pawn_can_be_enpassant.turn_ctr == 0 && 
+    Math.abs(dest_board.y-pawn_can_be_enpassant.y) == 1 &&
+    dest_board.x == pawn_can_be_enpassant.x) {
+      if(real_move) chess_board[pawn_can_be_enpassant.y][pawn_can_be_enpassant.x] = "__";
+      return true;
+    }
 
   // First move can move two space
   if(x_dist == 0 && y_start == 6 && y_dist == 2) {
-    pawn_can_be_enpassant.x = dest_board.x;
-    pawn_can_be_enpassant.y = dest_board.y;
-    pawn_can_be_enpassant.turn_ctr = 1;
+    if(real_move){
+      pawn_can_be_enpassant.x = dest_board.x;
+      pawn_can_be_enpassant.y = dest_board.y;
+      pawn_can_be_enpassant.turn_ctr = 1;
+    }
     return true;
   }
-  
   return false;
 }
 
-function moveKnight(x_dist, y_dist, dest_board){
+function moveKnight(x_dist, y_dist){
+  if(Math.abs(x_dist) == 1 && Math.abs(y_dist) == 2 || Math.abs(x_dist) == 2 && Math.abs(y_dist) == 1) return true;
   return false;
 }
 
-function moveBishop(x_dist, y_dist, dest_board){
+function moveBishop(x_dist, y_dist){
+  if(Math.abs(x_dist) == Math.abs(y_dist)) return true;
   return false;
 }
 
-function moveRook(x_dist, y_dist, dest_board){
-
+function moveRook(x_dist, y_dist){
+  if(x_dist == 0 && y_dist != 0 || x_dist != 0 && y_dist == 0) return true;
   return false;
 }
 
-function moveQueen(x_dist, y_dist, dest_board){
+function moveQueen(x_dist, y_dist){
+  if(Math.abs(x_dist) == Math.abs(y_dist) || x_dist == 0 && y_dist != 0 || x_dist != 0 && y_dist == 0) return true;
   return false;
 }
 
-function moveKing(x_dist, y_dist, dest_board){
-  
+function moveKing(x_dist, y_dist, color = "_", real_move=true){
+  if(Math.abs(x_dist) <= 1 && Math.abs(y_dist) <= 1) return true;
+  if(!real_move) return false;
+  // Castle
+  if(color == "W" && x_dist == 2 && white_castle.left) {
+    if(isSelfCheck("WK", {x:4, y:7}, {x:3, y:7})) return false;
+    chess_board[7][0] = "__";
+    chess_board[7][3] = "WR";
+    return true;
+  }
+  if(color == "W" && x_dist == -2 && white_castle.right) {
+    if(isSelfCheck("WK", {x:4, y:7}, {x:5, y:7})) return false;
+    chess_board[7][7] = "__";
+    chess_board[7][5] = "WR";
+    return true;
+  }
+  if(color == "B" && x_dist == -2 && black_castle.left) {
+    if(isSelfCheck("BK", {x:4, y:0}, {x:3, y:0})) return false;
+    chess_board[0][0] = "__";
+    chess_board[0][3] = "BR";
+    return true;
+  }
+  if(color == "B" && x_dist == 2 && black_castle.right) {
+    if(isSelfCheck("BK", {x:4, y:0}, {x:5, y:0})) return false;
+    chess_board[0][7] = "__";
+    chess_board[0][5] = "BR";
+    return true;
+  }
   return false;
 }
 
-
+// Gets absolute board coords
 function getBoardCoords(color, x, y){
   if(color == "B") return {x: 7-x, y: 7-y};
   return {x: x, y: y};
 }
 
-
-
 // isSpacesEmpty checks all diagonal, horizontal, or vertical spaces between start and destination,
 // then it checks if destination space doesn't have a same color piece on it,
-// all L moves only check the destination space
-function isSpacesEmpty(color, start_board, dest_board){
+// all other moves only check the destination space
+function isSpacesEmpty(color, start_board, dest_board, board_state = chess_board){
   let x_dist = start_board.x-dest_board.x;
   let y_dist = start_board.y-dest_board.y;
   if(Math.abs(x_dist) == Math.abs(y_dist) || x_dist == 0 && y_dist != 0 || x_dist != 0 && y_dist == 0){
@@ -172,79 +213,156 @@ function isSpacesEmpty(color, start_board, dest_board){
       let x_pos = dest_board.x+x_dist;
       let y_pos = dest_board.y+y_dist;
       if(x_dist == 0 && y_dist == 0) break;
-      if(chess_board[y_pos][x_pos] != "__") return false;
+      if(board_state[y_pos][x_pos] != "__") return false;
     }
   }
-  if(chess_board[dest_board.y][dest_board.x][0] == color) return false;
+  if(board_state[dest_board.y][dest_board.x][0] == color) return false;
   return true;
 }
 
-// isSelfCheck checks the color's king to see if any enemy pieces will
-// be able to check if the move is made
-function isSelfCheck(color, start_board, dest_board){
+// isSelfCheck checks the color's king to see if any 
+// enemy pieces will check if the move is made
+function isSelfCheck(piece, start_board = null, dest_board = null){
+  let color = piece[0];
+  let king_pos = {x:NaN, y:NaN};
+  let future_board = [];
+  let other_color = "W";
+  if(color == "W") other_color = "B";
 
+  for(let i=0; i<8; i++){
+    future_board.push(chess_board[i].slice());
+  }
+
+  if(start_board != null && dest_board != null){
+    future_board[start_board.y][start_board.x] = "__";
+    future_board[dest_board.y][dest_board.x] = piece; 
+  }
+
+  for(let i=0; i<8; i++){
+    for(let j=0; j<8; j++){
+      if(color+"K" == future_board[i][j]) {
+        king_pos.x = j; 
+        king_pos.y = i;
+      }
+    }
+  }
+
+  for(let i=0; i<8; i++){
+    for(let j=0; j<8; j++){
+      if(other_color == future_board[i][j][0]) {
+        let x_dist = j-king_pos.x;
+        let y_dist = i-king_pos.y;
+        if(other_color == "B"){
+          x_dist *= -1;
+          y_dist *= -1;
+        }
+        if(isSpacesEmpty(other_color, {x:j, y:i}, king_pos, future_board) && 
+          (future_board[i][j] == other_color+"Q" && moveQueen(x_dist, y_dist) ||
+          future_board[i][j] == other_color+"R" && moveRook(x_dist,y_dist) ||
+          future_board[i][j] == other_color+"B" && moveBishop(x_dist,y_dist) ||
+          future_board[i][j] == other_color+"N" && moveKnight(x_dist,y_dist) ||
+          future_board[i][j] == other_color+"K" && moveKing(x_dist,y_dist) ||
+          future_board[i][j] == other_color+"P" && Math.abs(x_dist) == 1 && y_dist == 1)) return true;
+      }
+    }
+  }
+  return false;
 }
 
+// isEnemyCheckmate checks if enemy is checkmated
+function isEnemyCheckmate(self_color){
+  let enemy_color = "W";
+  let xyOffset = 0;
+  if(self_color == enemy_color) {
+    xyOffset = 7;
+    enemy_color = "B";
+  }
+  if(!isSelfCheck(enemy_color+"_")) return false;
 
+  // yep check every piece of black going into 
+  // all 64 spaces to see if it stops the check
+  for(let i=0; i<8; i++){
+    for(let j=0; j<8; j++){
+      if(enemy_color == chess_board[i][j][0]) {
+        let ghost_piece = {piece: chess_board[i][j], x: Math.abs(xyOffset-j), y: Math.abs(xyOffset-i)};
+        for(let y=0; y<8; y++){
+          for(let x=0; x<8; x++){
+            if(move(ghost_piece, Math.abs(xyOffset-x), Math.abs(xyOffset-y), false)) return false;
+          }
+        }
+      }
+    }
+  }
+  checkmated = enemy_color;
+  return true;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// Socket.io stuff
+//
 io.on("connection", (socket) => {
-  // assign colors
+  // Assign colors
   if(assigned_colors.length < 2){
-    let color = "W"
-    if(assigned_colors.length == 1)
-      if(assigned_colors[0].color == "W") color = "B"
-    assigned_colors.push({id:socket.id, color:color})
+    let color = "W";
+    if(assigned_colors.length == 1 && assigned_colors[0].color == "W") color = "B";
+    assigned_colors.push({id:socket.id, color:color});
     socket.emit("assign color", color);
     console.log("a player connected:", assigned_colors[assigned_colors.length-1]);
   }
-
-  // setup game
+  
+  // Setup game
   io.emit("game update", chess_board);
+  io.emit("checkmate", checkmated);
 
-  // update after each move
-  socket.on("game update", (ghost_piece_rec, x_dest, y_dest) => {
-    if(!move(ghost_piece_rec, x_dest, y_dest)) {
+  // Update after each move
+  socket.on("game update", (ghost_piece, x_dest, y_dest) => {
+    let color = ghost_piece.piece[0];
+    if(assigned_colors.length != 2 || !move(ghost_piece, x_dest, y_dest) || color != turn) {
       io.emit("game update", chess_board);
       return;
     }
-    let color = ghost_piece_rec.piece[0];
+    pawn_can_be_enpassant.turn_ctr--;
     let dest_board = getBoardCoords(color, x_dest, y_dest);
-    let ghost_board = getBoardCoords(color, ghost_piece_rec.x, ghost_piece_rec.y);
-    chess_board[dest_board.y][dest_board.x] = ghost_piece_rec.piece;
-    chess_board[ghost_board.y][ghost_board.x] = "__";
+    let start_board = getBoardCoords(color, ghost_piece.x, ghost_piece.y);
+    chess_board[dest_board.y][dest_board.x] = ghost_piece.piece;
+    chess_board[start_board.y][start_board.x] = "__";
 
+    // Special promote function
+    if(ghost_piece.piece[1] == "P" && y_dest == 0) socket.emit("promote pawn", x_dest, y_dest);
+    else if(turn == "W") turn = "B";
+    else if(turn == "B") turn = "W";
     io.emit("game update", chess_board);
+
+    if(isEnemyCheckmate(color)) io.emit("checkmate", checkmated);
   });
 
-  // handle disconnects 
+  // Promote pawn
+  socket.on("promote pawn", (promote_piece) => {
+    let color = promote_piece.piece[0];
+    let promote_board = getBoardCoords(color, promote_piece.x, promote_piece.y);
+    chess_board[promote_board.y][promote_board.x] = promote_piece.piece;
+    io.emit("game update", chess_board);
+    if(turn == "W") turn = "B";
+    else if(turn == "B") turn = "W";
+
+    if(isEnemyCheckmate(color)) io.emit("checkmate", checkmated);
+  });
+
+  // Handle disconnects 
   socket.on("disconnect", () => {
-    if(assigned_colors.length > 0 && assigned_colors[0].id == socket.id){
+    if(assigned_colors.length == 1){
+      assigned_colors.length = 0;
+      console.log("player disconnected:", socket.id);
+    }
+    else if(assigned_colors.length == 2 && assigned_colors[0].id == socket.id){
       assigned_colors[0] = assigned_colors[1];
       assigned_colors.length = 1;
       console.log("player disconnected:", socket.id);
     }
-    else if(assigned_colors.length > 1 && assigned_colors[1].id == socket.id){
+    else if(assigned_colors.length == 2 && assigned_colors[1].id == socket.id){
       assigned_colors.length = 1;
       console.log("player disconnected:", socket.id);
     }
-
   });
 });
 
--- public/chess.js
+++ public/chess.js
@@ -6,6 +6,8 @@ var is_down = false;
 var mouse_x = 0;
 var mouse_y = 0;
 var client_color;
+var pawn_promoted = false;
+var checkmated = "_";
 
 // ghost = piece was there but is currently grabbed by mouse
 const ghost_piece = {
@@ -14,22 +16,31 @@ const ghost_piece = {
     piece: "__",
 };
 
+const promote_piece = {
+    x: NaN,
+    y: NaN,
+    piece: "__",
+}
+
 const TILE_SIZE = 80;
 const BOARD_SIZE = TILE_SIZE*8;
 const BOARD_X = (canvas.width-BOARD_SIZE)/2;
 const BOARD_Y = (canvas.height-BOARD_SIZE)/2;
+
 const IMG_SIZE = 42;
 const IMG_SCALE = TILE_SIZE*0.84;
 const IMG_OFF_CENTER = (TILE_SIZE-IMG_SCALE)/2;
 
-// promote();
-// check();
-// checkmate();
+const P_TILE_SIZE = TILE_SIZE*1.25;
+const P_BOARD_X = 2.75*TILE_SIZE+BOARD_X;
+const P_BOARD_Y = 2.75*TILE_SIZE+BOARD_Y;
+const P_OFF_CENTER = IMG_OFF_CENTER*2.5;
 
 // "__" = no piece
 // "BQ" = black queen
 // "WP" = white pawn
-const chess_board = []
+const chess_board = [];
+
 
 
 function getOffsets(piece){
@@ -68,6 +79,7 @@ function draw(){
         for(let j=0; j<8; j++){
             if((i+j)%2 == 0) ctx.fillStyle = "#d1d0cb";
             else ctx.fillStyle = "#1c8a2a";
+            ctx.beginPath();
             ctx.fillRect(j*TILE_SIZE+BOARD_X,i*TILE_SIZE+BOARD_Y,TILE_SIZE,TILE_SIZE);
         }
     }
@@ -88,26 +100,97 @@ function draw(){
     }
     
     // Draw grabbed piece
-    if(ghost_piece.piece == "__") return;
-    let offsets = getOffsets(ghost_piece.piece);
-    ctx.drawImage(document.getElementById("pieces"), 
-        offsets[0], offsets[1], 
-        IMG_SIZE, IMG_SIZE, 
-        mouse_x-IMG_SCALE/2, mouse_y-IMG_SCALE/2, 
-        IMG_SCALE, IMG_SCALE);
+    if(ghost_piece.piece != "__"){
+        let offsets = getOffsets(ghost_piece.piece);
+        ctx.drawImage(document.getElementById("pieces"), 
+            offsets[0], offsets[1], 
+            IMG_SIZE, IMG_SIZE, 
+            mouse_x-IMG_SCALE/2, mouse_y-IMG_SCALE/2, 
+            IMG_SCALE, IMG_SCALE);
+    }
+
+    // Draw pawn promotion
+    if(pawn_promoted){
+        let knight_offsets = getOffsets(client_color+"N");
+        let bishop_offsets = getOffsets(client_color+"B");
+        let rook_offsets = getOffsets(client_color+"R");
+        let queen_offsets = getOffsets(client_color+"Q");
+        let arr_offsets = [rook_offsets, knight_offsets, bishop_offsets, queen_offsets];
+
+        ctx.beginPath();
+        ctx.fillStyle = "#d1d0cb";
+        ctx.strokeStyle = "#000000";
+        ctx.lineWidth = 3;
+        ctx.roundRect(P_BOARD_X, P_BOARD_Y, P_TILE_SIZE*2, P_TILE_SIZE*2, 5);
+        ctx.stroke();
+        ctx.fill();
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.moveTo(P_TILE_SIZE+P_BOARD_X, P_BOARD_Y);
+        ctx.lineTo(P_TILE_SIZE+P_BOARD_X, 2*P_TILE_SIZE+P_BOARD_Y);
+        ctx.moveTo(P_BOARD_X, P_TILE_SIZE+P_BOARD_Y);
+        ctx.lineTo(2*P_TILE_SIZE+P_BOARD_X, P_TILE_SIZE+P_BOARD_Y);
+        ctx.stroke();
+
+        for(let i=0; i<4; i++){
+            ctx.drawImage(document.getElementById("pieces"), 
+                arr_offsets[i][0], arr_offsets[i][1], 
+                IMG_SIZE, IMG_SIZE, 
+                i%2*P_TILE_SIZE+P_BOARD_X+P_OFF_CENTER, (i+Math.floor(i/2))%2*P_TILE_SIZE+P_BOARD_Y+P_OFF_CENTER, 
+                IMG_SCALE, IMG_SCALE);
+        }
+    }
+
+    if(checkmated != "_"){
+        ctx.fillStyle = "#d1d0cb";
+        ctx.strokeStyle = "#000000";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.roundRect(P_BOARD_X-P_TILE_SIZE, P_BOARD_Y, P_TILE_SIZE*4, P_TILE_SIZE*2, 5);
+        ctx.stroke();
+        ctx.fill();
+
+        ctx.fillStyle = "#000000";
+        ctx.font = "48px Tickerbit";
+        if(client_color == checkmated) ctx.fillText("You Lose", P_BOARD_X, P_BOARD_Y+P_TILE_SIZE);
+        else ctx.fillText("You Win", P_BOARD_X, P_BOARD_Y+P_TILE_SIZE);
+    }
 }
 
 // Note: canvas coords are verticaly flipped on a chess board 
+// Mouse down handles picking the promoted piece
 function handleMouseDown(e){
     is_down = true;
     mouse_x = e.offsetX;
     mouse_y = e.offsetY;
+    if(pawn_promoted){
+        let x = Math.floor((mouse_x-P_BOARD_X)/P_TILE_SIZE);
+        let y = Math.floor((mouse_y-P_BOARD_Y)/P_TILE_SIZE);
+        if(x == 1 || x == 0 && y == 0 || y == 1) {
+            switch (x+y*2){
+                case 0:
+                    promote_piece.piece = client_color+"R";
+                    break;
+                case 1:
+                    promote_piece.piece = client_color+"Q";
+                    break;
+                case 2:
+                    promote_piece.piece = client_color+"B";
+                    break;
+                case 3:
+                    promote_piece.piece = client_color+"N";
+                    break;
+            }
+            pawn_promoted = false;
+            socket.emit("promote pawn", promote_piece);
+        }
+    }
 
     let x = Math.floor((mouse_x-BOARD_X)/TILE_SIZE);
     let y = Math.floor((mouse_y-BOARD_Y)/TILE_SIZE);
     if(x < 0 || x > 7 || y < 0 || y > 7) return;
     let piece = chess_board[y][x];
-    if(piece == "__" || piece[0] != client_color) return;
+    if(piece == "__" || piece[0] != client_color || checkmated != "_") return;
     ghost_piece.x = x;
     ghost_piece.y = y;
     ghost_piece.piece = piece;
@@ -146,11 +229,10 @@ function loop(){
 }
 loop();
 
-// update chess board
-socket.on("game update", (chess_board_rec) => {
+socket.on("game update", (chess_board_server) => {
     chess_board.length = 0;
     for(let i=0; i<8; i++){
-        chess_board.push(chess_board_rec[i]);
+        chess_board.push(chess_board_server[i]);
     }
     if(client_color == "B"){
         chess_board.reverse();
@@ -160,11 +242,19 @@ socket.on("game update", (chess_board_rec) => {
     }
 });
 
-// assign the side client plays as
 socket.on("assign color", (assigned_color) => {
     client_color = assigned_color;
-    console.log(assigned_color)
 })
+
+socket.on("promote pawn", (x, y) => {
+    pawn_promoted = true;
+    promote_piece.x = x;
+    promote_piece.y = y;
+});
+
+socket.on("checkmate", (checkmated_color) => {
+    checkmated = checkmated_color;
+});
 
 canvas.onmousedown = handleMouseDown;
 canvas.onmouseup = handleMouseUp;
